b6194339388b50aa69f43110cfc8b8b9eaac4441
Manuel <tabeojong23@gmail.com>
2025-02-07 09:47:20 +0100

    Update class Client

--- src/main/java/twisk/ClientTwisk.java
+++ src/main/java/twisk/ClientTwisk.java
@@ -6,11 +6,20 @@ import main.java.twisk.simulation.*;
 import java.util.Scanner;
 
 public class ClientTwisk {
+    /**
+     * Function pour ajouter un successeur au dernier etape du monde
+     */
+    private static void addLastSuccesseur(Monde world, Etape etape){
+        //Adjust this if the last Entree (predecessor) was an E/S (add the condition)
+        if (world.getLesEtapes().nbEtapes() > 0) {
+            world.getLesEtapes().lastEtape().ajouterSuccesseur(etape);
+        }
+    }
 
     /**
      * Fonction pour creer un monde
      */
-    public static Monde initMonde() {
+    private static Monde initMonde() {
         Scanner scanner = new Scanner(System.in);
 
         /**
@@ -24,7 +33,7 @@ public class ClientTwisk {
         System.out.println("Quel nom donnez-vous à ce monde?");
         String nomDuMonde = scanner.nextLine();
 
-        System.out.println("Combien d'Activités voulez-vous dans " + nomDuMonde + "?");
+        System.out.println("Combien d'Activités voulez-vous dans " + nomDuMonde + " (Activités restreintes inclus)?");
         int nombreActivities = scanner.nextInt();
 
         //Creer le monde
@@ -53,9 +62,9 @@ public class ClientTwisk {
                     String nomGuichet = scanner.next();
 
                     //Ask the Prof of this implantation
-                    System.out.println("Voulez vous que " + nom + " soit une Sortie(O/N)?");
+                    System.out.println("Voulez vous que " + nom + " soit une Entrée(O/N)?");
                     String activSoitSortie = scanner.next();
-                    System.out.println("Voulez vous que " + nomGuichet + " soit Une Entrée(O/N)?");
+                    System.out.println("Voulez vous que " + nomGuichet + " soit Une Sortie(O/N)?");
                     String GuichetSoitEntree = scanner.next();
 
                     //Ajout d'avtivité dans le monde
@@ -63,18 +72,77 @@ public class ClientTwisk {
                     Etape guichet = new Guichet(nomGuichet, limit);
                     guichet.ajouterSuccesseur(activ);
 
-                    //definir un predecesseur s'il y'avait un Etape deja dans monde
-                    if (world.getLesEtapes().nbEtapes() > 0) {
-                        world.getLesEtapes().lastEtape().ajouterSuccesseur(guichet);
+
+                    //cas ou un activité à plusieurs successeur
+                    if(world.nbEtapes() >= 2){
+                        System.out.println("Voulez vous que " + nom + " soit liée à un activité déja avec un successeur(O/N)?");
+                        String birfucation = scanner.next();
+                        if(birfucation.equals("O")){
+                            System.out.println("Quel est l'index de cette activité? (Les activités sont de 1-index) ");
+                            int indexactivite = scanner.nextInt();
+                            for(Etape e: world.getLesEtapes()){
+                                if(e.getNumeroEtape() == (indexactivite -1) && !e.estUnGuichet()){  //LEMME 4
+                                    e.ajouterSuccesseur(guichet);
+                                }
+                            }
+                        }
+                        else{
+                            if (birfucation.equals("N")){
+                                //ajouter au dernier element du monde
+                                addLastSuccesseur(world,guichet);
+                            }
+                        }
                     }
+                    else{
+                        //ajouter au dernier element du monde
+                        addLastSuccesseur(world,guichet);
+                    }
+
                     world.ajouter(guichet, activ);
                 }
 
-
+                //Cas d'activité normal
                 else {
-                    //Cas d'activité normal
                     if (choixRes.equals("N")) {
+                        System.out.println("Combien de temps reste un participant dans " + nom + " (en secondes)?");
+                        int time = scanner.nextInt();
+                        System.out.println("Avec l'écart(en secondes)? ");
+                        int ecart = scanner.nextInt();
 
+                        //Ask the Prof of this implantation
+                        System.out.println("Voulez vous que " + nom + " soit une Entrée(O/N)?");
+                        String activSoitSortie = scanner.next();
+                        System.out.println("Voulez vous que " + nom + " soit Une Sortie(O/N)?");
+                        String activSoitEntree = scanner.next();
+
+                        //Ajout d'activité dans le monde
+                        Etape activ = new Activite(nom);
+
+                        //cas ou un activité à plusieurs successeur
+                        if(world.nbEtapes() >= 2){
+                            System.out.println("Voulez vous que " + nom + " soit liée à un activité déja avec un successeur(O/N)?");
+                            String birfucation = scanner.next();
+                            if(birfucation.equals("0")){
+                                System.out.println("Quel est l'index de cette activité? (Les activités sont de 1-index)");
+                                int indexactivite = scanner.nextInt();
+                                for(Etape e: world.getLesEtapes()){
+                                    if(e.getNumeroEtape() == (indexactivite - 1) && !e.estUnGuichet()){  //LEMME 4
+                                        e.ajouterSuccesseur(activ);
+                                    }
+                                }
+                            }
+                            else{
+                                if(birfucation.equals("N")){
+                                    //ajouter au dernier element du monde
+                                    addLastSuccesseur(world,activ);
+                                }
+                            }
+                        }
+                        else{
+                            //ajouter au dernier element du monde
+                            addLastSuccesseur(world,activ);
+                        }
+                        world.ajouter(activ);
                     }
                 }
 
--- src/main/java/twisk/monde/Monde.java
+++ src/main/java/twisk/monde/Monde.java
@@ -1,34 +1,65 @@
 package main.java.twisk.monde;
 
+import java.util.Collections;
 import java.util.Iterator;
 
 public class Monde implements Iterable<Etape> {
-    GestionnaireEtapes lesEtapes ;
-    SasEntree entree;
-    SasSortie sortie;
+    private GestionnaireEtapes lesEtapes ;
+    private SasEntree entree;
+    private SasSortie sortie;
 
+    /**
+     * Constructeur
+     */
     public Monde() {
         this.lesEtapes = new GestionnaireEtapes();
         this.entree = new SasEntree();
         this.sortie = new SasSortie();
     }
 
-    @Override
-    public Iterator<Etape> iterator() {
-        return lesEtapes.iterator();
+    /**
+     * Function qui gère les Etapes qui sont des Entrées
+     * @param e les etapes passé en paramètre
+     */
+    public void aCommeEntree(Etape... e) {
+        //Ask the Prof
     }
 
+    /**
+     * Function qui gère les Etapes qui sont des Sortoes
+     * @param e les etapes passé en paramètre
+     */
+    public void aCommeSortie(Etape... e) {
+        //Ask the prof
+    }
+
+    /**
+     * @return la collection d'etapes dans le monde
+     */
     public GestionnaireEtapes getLesEtapes() {
         return lesEtapes;
     }
 
+    /**
+     * Fonction qui ajout les etapes dans un monde
+     * @param etape les etape(s) à ajouter
+     */
     public void ajouter(Etape... etape) {
-
         lesEtapes.ajouterEtape(etape);
     }
+
+    /**
+     * Function qui compte le nombre d'etapes dans un monde
+     * @return le nombre d'etapes
+     */
     public int nbEtapes(){
         return lesEtapes.nbEtapes();
     }
+
+    /**
+     * Function qui compte le nombre de guichets dans un monde
+     * @return le nombre de guichets
+     */
     public int nbGuichet() {
         int nbEtapes = 0;
         for (Etape etape : lesEtapes) {
@@ -39,5 +70,13 @@ public class Monde implements Iterable<Etape> {
 
         }
         return nbEtapes;
+    }
+
+    /**
+     * Iterator
+     */
+    @Override
+    public Iterator<Etape> iterator() {
+        return lesEtapes.iterator();
     }
 }
