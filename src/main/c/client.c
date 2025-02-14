6098789e04f84cbe3d29f37dce7483abd016e703
Ayata BayramEfe <bayram-efe.ayata4@etu.univ-lorraine.fr>
2025-02-14 10:48:51 +0100

    Fonction simulation

--- src/main/c/client.c
+++ src/main/c/client.c
@@ -1,5 +1,15 @@
-#include "DEF.h"
+#include "../ressources/codeC/def.h"
+
+#define sasEntree 0
+#define sasSortie 1
+#define activite 2
+
 
 void simulation (int ids){
+  entrer(sasEntree);
+  delai(7, 3);
+  transfert(0,1);
+  delai(4, 2);
+  transfert(1,2);
 
-}
+    }
