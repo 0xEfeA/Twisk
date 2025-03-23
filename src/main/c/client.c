8fd12c0e2cb1eb4356daaac20e00b80aa24f2237
Ayata BayramEfe <bayram-efe.ayata4@etu.univ-lorraine.fr>
2025-03-23 16:50:16 +0100

    Revert de client.c qui avait était modifié pour du debugging de Simulation

--- src/main/c/client.c
+++ src/main/c/client.c
@@ -1,28 +1,82 @@
 #include "../ressources/codeC/def.h"
-#define SASENTREE 0
-#define SASSORTIE 1
-#define ZOO 2
-#define GUICHET_TOBOGAN 3
-#define TOBOGAN 4
-#define PISCINE 5
 
-void simulation(int ids){
-entrer(SASENTREE);
-delai(4,2);
-transfert(SASENTREE,ZOO);
+#define sasEntree 0
+#define sasSortie 5
+#define activiteRestreinte 4
+#define activite 2
+#define guichet 1
+#define guichet2 3
 
-delai(6,3);
-transfert(ZOO,GUICHET_TOBOGAN);
+#define num_sem_guichet 1
+#define num_sem_guichet2 1
 
-P(ids,1);
-delai(4,2);
-V(ids,1);
-transfert(GUICHET_TOBOGAN,TOBOGAN);
+#define numero_etape 0
 
-delai(4,2);
-transfert(TOBOGAN,PISCINE);
 
-delai(5,2);
-transfert(PISCINE,SASSORTIE);
+void simulation (int ids){
 
-}
+
+  entrer(sasEntree);
+  delai(4,1);
+
+
+  //guichet
+  transfert(sasEntree,guichet);
+  P(ids,num_sem_guichet);
+  delai(4,1);
+
+  //activité
+  transfert(guichet,activite);
+
+
+
+  delai(5, 1);
+  V(ids,num_sem_guichet);
+  /**
+*  Instruction à ajouter pour nouvelle activité (question 3 Séance 4) :
+*  transfert(activitePredecesseur, activiteNouvelle);
+*  delai(3,1);
+*  transfert(activiteNouvelle, Successeurs : {sasSortie ou guichet ou activité});
+*/
+  /** Ancien code question 3 séance 4
+  *  transfert(activite,activite2);
+  *  delai(5, 1);
+  *  //sasSortie
+  *  transfert(activite2,sasSortie);
+*/
+
+  /**
+* Instructions pour ajouter Activité restreinte (Question 4 séance 4)
+  *   transfert(predecesseur, guichetNouveau);
+ *     P(ids, num_sem_guichetNouveau);
+ *     delai(3,1);
+ *     transfert(guichetNouveau, activiteRestreinte);
+ *     delai(4,1);
+ *      V(ids, num_sem_guichetNouveau);
+ *     transfert(activiteRestreinte,successeurs)
+*/
+  /** Instructions SasEntree et SasSortie (question 5 Séance 4)
+* SasEntree: entrer(sasEntree);
+* SasSortie : transfert(activite , sasSortie);
+*/
+
+ /** Question 6 séance 4
+ *  Partie variante du code (dépend du monde):
+*     -Les constantes : #define activite...
+*     -Les transfert : dépends des liens entre le activité et guichet choisi par celui qui créer le monde
+*     -les délais
+*
+*   Partie fixe du code :
+*   - Entrée au début du monde
+*   - Sortie à la fin du monde
+*/
+  transfert(activite, guichet2);
+  P(ids, num_sem_guichet2);
+  delai(3,1);
+  transfert(guichet2, activiteRestreinte);
+  delai(4,1);
+  V(ids, num_sem_guichet2);
+
+  //sasSortie
+  transfert(activiteRestreinte,sasSortie);
+    }
