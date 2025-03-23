e7b210cdc496831acd027ae3b2a8fc64dca16ded
Manuel <tabeojong23@gmail.com>
2025-03-23 12:44:32 +0100

    Fin séance 6, Function simuler à completer...

--- src/main/java/twisk/simulation/Simulation.java
+++ src/main/java/twisk/simulation/Simulation.java
@@ -3,11 +3,8 @@ package twisk.simulation;
 import twisk.monde.*;
 import twisk.outils.KitC;
 
-import java.util.ArrayList;
-
 public class Simulation {
     private KitC environnement;
-    private int nbClients = 5; // Par défaut on a 5 clients
     /**
      * Constructeur sans argument
      */
@@ -15,6 +12,11 @@ public class Simulation {
         this.environnement = new KitC();
         this.environnement.creerEnvironnement(); // Création du répertoire et copie des fichiers
     }
+
+    /**
+     * Affichage des details du monde en mode C
+     * @param monde
+     */
     public void simulerprint(Monde monde){
         System.out.println(monde.toC());
     }
@@ -28,66 +30,91 @@ public class Simulation {
         environnement.creerFichier(codeC);
         environnement.compiler();
         environnement.construireLaBibliotheque();
-        System.load("/tmp/twisk/libTwisk.so") ;
+        System.load("/tmp/twisk/libTwisk.so");
 
         // Instanciations des paramètres du monde de test
         int nbEtapes = monde.nbEtapes();
-        int nbClients = this.nbClients;
-        int nbGuichets =monde.nbGuichet();
+        int nbClients = 10;
+        int nbGuichets = monde.nbGuichet();
         int[] tabJetonsGuichet = new int[nbGuichets];
-        //nombre de jetons pour chaque guichets
-        ArrayList<Guichet> guichets = monde.getGuichets();
-        for(Guichet gui: guichets){
-            tabJetonsGuichet[gui.getNumeroSemaphore()-1] = gui.getNbJetons();
-        }
-        // Récupération de l'adresse du tableau contenant les pid des processus retourné par start_simulation
+
+        if (nbGuichets > 0) tabJetonsGuichet[0] = 1;
+        if (nbGuichets > 1) tabJetonsGuichet[1] = 1;
+
+        // Récupération du tableau des PIDs des processus simulés
         int[] tabsimu = start_simulation(nbEtapes, nbGuichets, nbClients, tabJetonsGuichet);
-        // Affichage pid
-        System.out.print("Les clients :");
+
+        // Affichage des PIDs des clients
+        System.out.print("Les clients : ");
         for (int i = 0; i < nbClients; i++) {
-            System.out.printf(" %d, ",tabsimu[i]);
-
+            System.out.printf(" %d, ", tabsimu[i]);
         }
-        System.out.print("\n");
+        System.out.println("\n");
 
-        //Boucle inifinie qui sera fini que quand condition d'arrêt réalisée
-        while (1==1) {
-            // Récupération des informations des clients
+        // Boucle de simulation
+        while (true) {
             int[] tabclient = ou_sont_les_clients(nbEtapes, nbClients);
-            // taille de ségment mémoire d'une étape (nbClient + 1 case qui stock le nombre de client en mémoire)
-            int tailleEtapesEnMemoire = nbClients+1;
+            int tailleEtapesEnMemoire = nbClients + 1;
+
             for (int i = 0; i < nbEtapes; i++) {
-                // On récupère le nombre de client de l'étape i
                 int nbClientEtapeI = tabclient[i * tailleEtapesEnMemoire];
-                // Selon le numéro l'étape on affiche un nom différent
-                System.out.printf("étape %d (%s) %d clients : ", i, monde.getNomEtape(i), nbClientEtapeI);
-                //Affichage des identifiants des clients à l'étape i
-                for (int j = 0; j < nbClientEtapeI; j++) {
-                    int pidClientEtapeI = tabclient[i *tailleEtapesEnMemoire + j + 1];
-                    System.out.printf("%d,",pidClientEtapeI);
+
+                // Trouver l'étpe correspondante
+                Etape etape = null;
+                for (Etape e : monde.getLesEtapes()) {
+                    if (e.getNumeroEtape() == i) {
+                        etape = e;
+                        break;
+                    }
                 }
 
-                System.out.print("\n");
+                // Déterminer le type d'étape
+                String typeEtape = "Pas d'nom"; //par defaut
+                if (etape != null) {
+                    if (etape.estSasEntree()) {
+                        typeEtape = "Entrée";
+                    }
+                   else if (etape.estUnGuichet()) {
+                        typeEtape = etape.getNom();
+                    }else if (etape.estUneActivite()) {
+                        typeEtape = etape.getNom();
+                    } else if (etape.estSasSortie()) {
+                    typeEtape = "Sortie";
+                    }
+                }
+
+                System.out.printf("étape %d (%s) %d clients : ", i, typeEtape, nbClientEtapeI);
+
+                // Affichage des identifiants des clients
+                for (int j = 0; j < nbClientEtapeI; j++) {
+                    int pidClientEtapeI = tabclient[i * tailleEtapesEnMemoire + j + 1];
+                    System.out.printf("%d,", pidClientEtapeI);
+                }
+
+                System.out.println();
             }
-            //Condition d'arrêt : quand tous les clients arrivent à la sortie
-            if ( tabclient[(nbEtapes-1) * tailleEtapesEnMemoire]==nbClients) {
+
+            // Condition d'arrêt : Tous les clients sont arrivés à la sortie
+            if (tabclient[(nbEtapes - 1) * tailleEtapesEnMemoire] == nbClients) {
                 break;
             }
 
+            // Pause d'une seconde
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                System.err.println("Interruption détectée, arrêt de la simulation.");
+                Thread.currentThread().interrupt();
+                break;
             }
 
-            System.out.print("\n");
+            System.out.println();
         }
-        nettoyage();
 
+        nettoyage();
     }
-    public void setNbClients(int nbClients) {
-        this.nbClients = nbClients;
-    }
+
+
     //Fonctions natives
     public native int[] start_simulation(int nbEtapes, int nbGuichets, int nbClients, int[] tabJetonsGuichets);
     public native int[] ou_sont_les_clients(int nbEtapes, int nbClients);
